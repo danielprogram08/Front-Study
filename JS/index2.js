082a9e96ab8fca1385ae1bef5c440f62e6d92c40
danielprogram08 <danieldanielsilva08@gmail.com>
2024-01-18 12:45:35 -0300

    Adicionado novo arquivo index.js!

---
+++ JS/index2.js
@@ -0,0 +1,8 @@
+//Declarando uma funcão;
+//OBS: Primeiro, declare a função, insira o produto que a função irá retornar, por fim, chame-a após declarar uma variável;
+function soma(valor, valor2) {
+    return valor + valor2;
+}
+
+let resultado = soma(5, 5);
+console.log(resultado);
